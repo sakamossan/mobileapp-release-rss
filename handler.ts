17886dfbbea6a76b47e6118f1c0be5e8c289d02f
yuji-hatakeyama <htkymyj0512@gmail.com>
2019-07-05 17:16:11 +0900

    [#6] Refactor GooglePlay Store scraping logic (for add iOS Store scraping).

--- handler.ts
+++ handler.ts
@@ -1,10 +1,10 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
-import { f } from './src/index';
+import { android_rss } from './src/android';
 
 export const MobileappReleaseRss: APIGatewayProxyHandler = async (event, _context) => {
-  const body = await f(event.queryStringParameters.android_app_id);
+  const body = await android_rss(event.queryStringParameters.android_app_id);
   return {
     statusCode: 200,
     body,
